20961f88c33e51344312a5835321a574d842e75d
murrayCai <murraycai@icloud.com>
2020-04-19 11:36:10 +0800

    init

---
+++ build/test/CMakeFiles/test_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/test_1"
+  "../../bin/test_1.pdb"
+  "CMakeFiles/test_1.dir/1.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
